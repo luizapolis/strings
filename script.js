2c291426b680c57b10a347ce52b8964b50f33fce
luizapolis <luiza.polis@escola.pr.gov.br>
2025-10-17 10:20:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('botao-palavrachaves');
+
+botaoMostraPalavras.addEventListener('click' ,mostraPalavrasChave); 
+
+function mostraPalavrasChave (){
+    alert('Fui clicado!');
+}
