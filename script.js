4dd3841144fca23b4d51876893cb224e86503002
luizapolis <luiza.polis@escola.pr.gov.br>
2025-11-28 10:19:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-import {PALAVRAS_RUINS} from "./palavrasRuins.js"
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavrasChave = processaTexto(texto);
+
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
     for (let i in palavras) {
         palavras[i] = palavras[i].toLowerCase();
     }
 
     palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
     function ordenaPalavra(p1, p2) {
         return frequencias[p2] - frequencias[p1];
     }
 
     return ordenadas.slice(0, 10);
-} 
+}
 
 function contaFrequencias(palavras) {
     let frequencias = {};
@@ -39,8 +43,7 @@ function contaFrequencias(palavras) {
 }
 
 function tiraPalavrasRuins(palavras) {
-const PALAVRAS_RUINS = new Set([
-    "que",
+    const PALAVRAS_RUINS = new Set (["que",
     "para",
     "com",
     "não",
@@ -161,13 +164,12 @@ const PALAVRAS_RUINS = new Set([
     "desde",
     "uma vez",
     "segundo",
-    "junto"
-]);
+    "junto"]);
     const palavrasBoas = [];
     for (let palavra of palavras) {
-      if(!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
-        palavrasBoas.push(palavra);
-      }
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
     }
-     return palavrasBoas;
+    return palavrasBoas;
 }
